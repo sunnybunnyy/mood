c4e0de5397db038546d0e8bf39f52cd2ada2d8cf
Radha Kotra <kradha6113@gmail.com>
2025-04-20 17:18:47 -0400

    Move customizations from tailwind.config.js to index.css

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import autoprefixer from 'autoprefixer'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,5 +13,11 @@ export default defineConfig({
         changeOrigin: true,
       },
     },
+  },
+  css: {
+      plugins: [
+        tailwindcss(),
+        autoprefixer(),
+      ],
   },
 });
