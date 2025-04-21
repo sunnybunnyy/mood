68a60e8d1173df7135eba7f43182eb938e6f6206
Radha Kotra <kradha6113@gmail.com>
2025-04-21 13:53:58 -0400

    Replace labels for email and password with placeholders

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -24,22 +24,22 @@ export default function Login() {
             {error && <div className='text-red-500 mb-4'>{error}</div>}
             <form onSubmit={handleSubmit}>
                 <div className='mb-4'>
-                    <label htmlFor="" className='block mb-2'>Email</label>
                     <input
                         type="email"
                         value={email}
                         onChange={(e) => setEmail(e.target.value)}
                         className="w-full p-2 border rounded"
+                        placeholder='Email'
                         required
                     />
                 </div>
                 <div className="mb-4">
-                    <label className="block mb-2"> Password</label>
                     <input
                         type="password"
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
                         className="w-full p-2 border rounded"
+                        placeholder='Password'
                         required
                     />
                 </div>
