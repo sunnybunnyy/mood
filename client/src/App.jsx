972f5960b4bdb644690e537abdc0128937ef1658
Radha Kotra <kradha6113@gmail.com>
2025-04-21 21:00:04 -0400

    Only display Navbar when not on the login or register page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import PrivateRoute from './components/PrivateRoute';
 import Dashboard from './pages/Dashboard';
@@ -9,22 +9,34 @@ import MoodEntry from './pages/MoodEntry';
 import Analytics from './pages/Analytics';
 import Navbar from './components/Navbar';
 
+function LayoutWithNavbar() {
+  return (
+    <>
+    <Navbar />
+    <div className='container mx-auto p-4'>
+      <Outlet />
+    </div>
+    </>
+  );
+}
+
 function App() {
   return (
     <Router>
       <AuthProvider>
-        <div className="min-h-screen bg-gray-50">
-          <div className="container mx-auto p-4">
-            <Routes>
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
+          <Routes>
+            {/* Routes with Navbar */}
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          
+            {/* Routes with Navbar */}
+            <Route element={<LayoutWithNavbar />}>
               <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
               <Route path="/" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
               <Route path="/entry" element={<PrivateRoute><MoodEntry /></PrivateRoute>} />
-              <Route path="analytics" element={<PrivateRoute><Analytics /></PrivateRoute>} />
-            </Routes>
-          </div>
-        </div>
+              <Route path="/analytics" element={<PrivateRoute><Analytics /></PrivateRoute>} />
+            </Route>
+          </Routes>
       </AuthProvider>
     </Router>
   );
